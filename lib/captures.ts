e8d36c27567f3ea01e18b447cacdcad0c24c8696
Conrad Buck <conartist6@gmail.com>
2021-01-14 11:27:20 -0500

    Missing captures undefined

--- lib/captures.ts
+++ lib/captures.ts
@@ -11,7 +11,7 @@ function _flattenCapture(capture: Capture, captures: Array<string | null>) {
 }
 
 export function flattenCapture(capture: Capture, capturesLen: number): Array<string | null> {
-  const captures = new Array(capturesLen).fill(null);
+  const captures = new Array(capturesLen).fill(undefined);
   _flattenCapture(capture, captures);
   return captures;
 }
