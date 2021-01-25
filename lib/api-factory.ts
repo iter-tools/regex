e9538b3188839c2a9503c0c4bb509bd43581e6de
Conrad Buck <conartist6@gmail.com>
2021-01-24 21:06:08 -0500

    regex literal input

--- lib/api-factory.ts
+++ lib/api-factory.ts
@@ -1,22 +1,22 @@
 import { parse } from './regex';
 
-import { Pattern } from './types';
+import { Pattern, PatternLike } from './types';
 
 export const apiFactory = <I>(
   generate: (pattern: Pattern, iterable: I) => IterableIterator<Array<string | null>>,
 ) => {
-  const exec = (pattern: string | Pattern, iterable: I) => {
-    const step = generate(typeof pattern === 'string' ? parse(pattern) : pattern, iterable).next();
+  const exec = (pattern: string | PatternLike, iterable: I) => {
+    const step = generate(parse(pattern), iterable).next();
 
     return step.done ? null : step.value;
   };
 
-  const test = (pattern: string, iterable: I) => {
+  const test = (pattern: string | PatternLike, iterable: I) => {
     return exec(pattern, iterable) !== null;
   };
 
-  const execGlobal = (pattern: string | Pattern, iterable: I) => {
-    return generate(typeof pattern === 'string' ? parse(pattern) : pattern, iterable);
+  const execGlobal = (pattern: string | PatternLike, iterable: I) => {
+    return generate(parse(pattern), iterable);
   };
 
   return { exec, test, execGlobal };
