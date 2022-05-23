f66bdbc3ed6bcd37b20b51a4aea7c2c9a3a052b3
Conrad Buck <conartist6@gmail.com>
2022-05-23 10:41:50 -0400

    Fix debugPrint exprs

--- lib/internal/debug.ts
+++ lib/internal/debug.ts
@@ -18,14 +18,15 @@ export const debugPrint = (matcher: any): string | null => {
       case 'edgeAssertion':
         str += m.kind === 'start' ? '^' : '$';
         break;
-      case 'expression':
-        str += `(${m.matchers.map((m: any) => debugPrint(m)).join('|')})`;
-        break;
       case 'repeat':
         if (m.repeatCont.name !== 'unmatched') {
           str += `(${m.exprCont.seqs.map((m: any) => debugPrint(m)).join('|')})*`;
         }
         break;
+      case 'expression':
+        str += `(${m.matchers.map((m: any) => debugPrint(m)).join('|')})`;
+        // m.next is already distributed into m.matchers -- don't print it twice!
+        return str;
       default:
         break;
     }
